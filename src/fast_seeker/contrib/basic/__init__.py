667fd3a9635679f38ea5a3d64af5bde9fb4deacd
Aryan Curiel <aryan.curiel@gmail.com>
2024-11-29 12:31:25 +0200

    Removed basic code

--- src/fast_seeker/contrib/basic/__init__.py
+++
@@ -1,3 +0,0 @@
-from .filtering import DictIterableFilterer, ObjectIterableFilterer
-from .pagination import BasicLimitOffsetPaginator, BasicPageNumberPaginator
-from .sorting import DictIterableSorter, ObjectIterableSorter
