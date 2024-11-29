b37a7cde2f7ea197b5b131cb491be7929ebb0820
Aryan Curiel <aryan.curiel@gmail.com>
2024-11-29 12:31:50 +0200

    Fixed a bug with resovler function in filtering

--- src/fast_seeker/contrib/beanie/filtering.py
+++ src/fast_seeker/contrib/beanie/filtering.py
@@ -7,14 +7,14 @@ class BeanieFilterer(Filterer[FindMany, FindMany]):
     def filter(self, data: FindMany, filter_query: FilterModel, *args, **kwargs) -> FindMany:
         filter_lookups = []
         for field_name, field_info in filter_query.model_fields.items():
-            resolver_func = getattr(self, f"resolve_{field_name}", None)
+            resolver_func = getattr(filter_query, f"resolve_{field_name}", None)
             field_extra = field_info.json_schema_extra or filter_query.model_config.get("json_schema_extra", {})
             ignore_none = field_extra.get("ignore_none", True)
             field_value = getattr(filter_query, field_name)
             if field_value is None and ignore_none:
                 continue
             new_lookup = (
-                resolver_func(filter_query, field_name)
+                resolver_func(field_value, field_info)
                 if callable(resolver_func)
                 else {field_name: getattr(filter_query, field_name)}
             )
