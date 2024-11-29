efa1ba0ebaf89a659379f14b6b8f8fe00f785ffa
Aryan Curiel <aryan.curiel@gmail.com>
2024-11-29 12:32:11 +0200

    Added tests

--- src/tests/test_contrib_beanie/test_contrib_beanie_filtering.py
+++ src/tests/test_contrib_beanie/test_contrib_beanie_filtering.py
@@ -1,3 +1,4 @@
+import pytest
 from pydantic import Field
 
 from fast_seeker.contrib.beanie.filtering import BeanieFilterer, FilterModel
@@ -10,22 +11,38 @@ class DummyFilterQueryWithoutNone(FilterModel):
     field2: str | None = None
 
 
-def test_beanie_filterer_filter__should_filter_data_and_ignore_none_by_default():
-    filterer = BeanieFilterer()
-    data = DummyFindMany()
-    filter_query = DummyFilterQueryWithoutNone(field1="value1")
-    result = filterer.filter(data, filter_query)
-    assert result.find_expressions == [{"field1": "value1"}]
-
-
 class DummyFilterQueryWithFieldNone(FilterModel):
     field1: str | None = None
     field2: str | None = Field(None, json_schema_extra={"ignore_none": False})
 
 
-def test_beanie_filterer_filter__should_filter_data_and_not_ignore_none_when_specified():
+class DummyFilterWithResolver(FilterModel):
+    field1: str | None = None
+    field2: str | None = None
+
+    def resolve_field1(self, field_value, field_info):
+        return {"field1": "custom_value"}
+
+
+class DummyFilterWithResolverNone(FilterModel):
+    field1: str | None = None
+    field2: str | None = None
+
+    def resolve_field1(self, field_value, field_info):
+        return None
+
+
+@pytest.mark.parametrize(
+    "filter_query,expected_expressions",
+    [
+        (DummyFilterQueryWithoutNone(field1="value1"), [{"field1": "value1"}]),
+        (DummyFilterQueryWithFieldNone(field1="value1"), [{"field1": "value1"}, {"field2": None}]),
+        (DummyFilterWithResolver(field1="value1"), [{"field1": "custom_value"}]),
+        (DummyFilterWithResolverNone(field1="value1"), []),
+    ],
+)
+def test_beanie_filterer_filter__should_assign_expected_find_expressions(filter_query, expected_expressions):
     filterer = BeanieFilterer()
     data = DummyFindMany()
-    filter_query = DummyFilterQueryWithFieldNone(field1="value1", field2=None)
     result = filterer.filter(data, filter_query)
-    assert result.find_expressions == [{"field1": "value1"}, {"field2": None}]
+    assert result.find_expressions == expected_expressions
