d927562ca9410f94a748cdaa6d8f9e4cb96838ba
Aryan Curiel <aryan.curiel@gmail.com>
2024-12-02 12:40:00 +0200

    Added filtering

---
+++ src/fast_seeker/contrib/odmantic/filtering.py
@@ -0,0 +1,31 @@
+from collections.abc import Iterable
+
+from odmantic import Model as ODManticModel
+from odmantic.query import QueryExpression
+
+from fast_seeker.core.filtering import Filterer, FilterModel
+
+
+class BeanieFilterer(Filterer[type[ODManticModel], Iterable[QueryExpression]]):
+    def filter(
+        self, data: type[ODManticModel], filter_query: FilterModel, *args, **kwargs
+    ) -> Iterable[QueryExpression]:
+        filter_lookups = []
+        for field_name, field_info in filter_query.model_fields.items():
+            resolver_func = getattr(filter_query, f"resolve_{field_name}", None)
+            field_extra = field_info.json_schema_extra or filter_query.model_config.get("json_schema_extra", {})
+            ignore_none = field_extra.get("ignore_none", True)
+            field_value = getattr(filter_query, field_name)
+            if field_value is None and ignore_none:
+                continue
+            new_lookup = (
+                resolver_func(field_value, field_info)
+                if callable(resolver_func)
+                else getattr(data, field_name) == field_value
+            )
+            if not new_lookup:
+                continue
+            if not isinstance(new_lookup, list):
+                new_lookup = [new_lookup]
+            filter_lookups.extend(new_lookup)
+        return filter_lookups
